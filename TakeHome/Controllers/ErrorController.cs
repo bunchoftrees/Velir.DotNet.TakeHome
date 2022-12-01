bf4934d6f0c72d5df7eb44c0c51ebeecd9dce2b2
Forrest JB Hartley <forrest@forrestbear.org>
2022-11-30 21:11:48 -0500

    Attempted fix on Error page not showing, incomplete

---
+++ TakeHome/Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace TakeHome.Controllers
+{
+	public class ErrorController : Controller
+    {
+		[HandleError]
+		public ActionResult Index()
+		{
+			return View("Error");
+		}
+	}
+}
+
